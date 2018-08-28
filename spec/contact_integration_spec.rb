52cf2d9f07df8dc6243e6a4ecc3021f031db2cd8
Roy Kim <rykim79@gmail.com>
2018-08-28 19:08:06 -0400

    refactor contact_integration_spec

--- spec/contact_integration_spec.rb
+++ spec/contact_integration_spec.rb
@@ -20,22 +20,12 @@ describe 'the contact path', :type => :feature do
   end
 
   it 'displays the contact in the list of contacts after submitting the form' do
-    # fill_in :first_name, :with => 'John'
-    # fill_in :last_name, :with => 'Doe'
-    # fill_in :job_title, :with => 'Manager'
-    # fill_in :company, :with => 'ACME Inc'
-    # fill_in :contact_type, :with => 'Business'
     click_button 'Add'
     expect(page).to have_css('li.contact', :text => 'John')
     expect(page).to have_css('li.contact', :text => 'Doe')
   end
 
   it 'displays the contacts in the list of contacts that have been saved' do
-    # fill_in :first_name, :with => 'John'
-    # fill_in :last_name, :with => 'Doe'
-    # fill_in :job_title, :with => 'Manager'
-    # fill_in :company, :with => 'ACME Inc'
-    # fill_in :contact_type, :with => 'Business'
     click_button 'Add'
     fill_in :first_name, :with => 'Mike'
     fill_in :last_name, :with => 'Smith'
@@ -50,11 +40,6 @@ describe 'the contact path', :type => :feature do
   end
 
   it 'will show a detailed view of a contact when it is clicked on' do
-    # fill_in :first_name, :with => 'John'
-    # fill_in :last_name, :with => 'Doe'
-    # fill_in :job_title, :with => 'Manager'
-    # fill_in :company, :with => 'ACME Inc'
-    # fill_in :contact_type, :with => 'Business'
     click_button 'Add'
     click_link 'John Doe'
     expect(page).to have_content('Job Title')
