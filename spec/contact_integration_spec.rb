504e84dabcc1c706defa0f19362ded63e1096aa2
Roy Kim <rykim79@gmail.com>
2018-08-28 19:05:23 -0400

    add integration for link brining user back to contacts view from detail view

--- spec/contact_integration_spec.rb
+++ spec/contact_integration_spec.rb
@@ -6,6 +6,11 @@ set :show_exceptions, false
 describe 'the contact path', :type => :feature do
   before :each do
     visit '/'
+    fill_in :first_name, :with => 'John'
+    fill_in :last_name, :with => 'Doe'
+    fill_in :job_title, :with => 'Manager'
+    fill_in :company, :with => 'ACME Inc'
+    fill_in :contact_type, :with => 'Business'
   end
 
   it 'displays an empty list of contacts and a form' do
@@ -15,22 +20,22 @@ describe 'the contact path', :type => :feature do
   end
 
   it 'displays the contact in the list of contacts after submitting the form' do
-    fill_in :first_name, :with => 'John'
-    fill_in :last_name, :with => 'Doe'
-    fill_in :job_title, :with => 'Manager'
-    fill_in :company, :with => 'ACME Inc'
-    fill_in :contact_type, :with => 'Business'
+    # fill_in :first_name, :with => 'John'
+    # fill_in :last_name, :with => 'Doe'
+    # fill_in :job_title, :with => 'Manager'
+    # fill_in :company, :with => 'ACME Inc'
+    # fill_in :contact_type, :with => 'Business'
     click_button 'Add'
     expect(page).to have_css('li.contact', :text => 'John')
     expect(page).to have_css('li.contact', :text => 'Doe')
   end
 
   it 'displays the contacts in the list of contacts that have been saved' do
-    fill_in :first_name, :with => 'John'
-    fill_in :last_name, :with => 'Doe'
-    fill_in :job_title, :with => 'Manager'
-    fill_in :company, :with => 'ACME Inc'
-    fill_in :contact_type, :with => 'Business'
+    # fill_in :first_name, :with => 'John'
+    # fill_in :last_name, :with => 'Doe'
+    # fill_in :job_title, :with => 'Manager'
+    # fill_in :company, :with => 'ACME Inc'
+    # fill_in :contact_type, :with => 'Business'
     click_button 'Add'
     fill_in :first_name, :with => 'Mike'
     fill_in :last_name, :with => 'Smith'
@@ -45,13 +50,21 @@ describe 'the contact path', :type => :feature do
   end
 
   it 'will show a detailed view of a contact when it is clicked on' do
-    fill_in :first_name, :with => 'John'
-    fill_in :last_name, :with => 'Doe'
-    fill_in :job_title, :with => 'Manager'
-    fill_in :company, :with => 'ACME Inc'
-    fill_in :contact_type, :with => 'Business'
+    # fill_in :first_name, :with => 'John'
+    # fill_in :last_name, :with => 'Doe'
+    # fill_in :job_title, :with => 'Manager'
+    # fill_in :company, :with => 'ACME Inc'
+    # fill_in :contact_type, :with => 'Business'
     click_button 'Add'
     click_link 'John Doe'
     expect(page).to have_content('Job Title')
+  end
+
+  it 'will have a link to get from the contact_detail to the main contacts view' do
+    click_button 'Add'
+    click_link 'John Doe'
+    click_link 'Back to contacts'
+    expect(page).to have_css('ul.contacts', :count => 1)
+    expect(page).to have_css('form', :count => 1)
   end
 end
