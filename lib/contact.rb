e32d043b33fce5f081bac69c63701988e5a4af35
Roy Kim <rykim79@gmail.com>
2018-08-28 14:06:38 -0400

    add #initialize()

--- lib/contact.rb
+++ lib/contact.rb
@@ -1,3 +1,11 @@
 class Contact
+  attr_accessor :first_name, :last_name, :job_title, :company, :contact_type
 
+  def initialize(attributes)
+    @first_name = attributes.fetch(:first_name)
+    @last_name = attributes.fetch(:last_name)
+    @job_title = attributes.fetch(:job_title)
+    @company = attributes.fetch(:company)
+    @contact_type = attributes.fetch(:contact_type)
+  end
 end
--- spec/contact_spec.rb
+++ spec/contact_spec.rb
@@ -3,5 +3,20 @@ require 'contact'
 
 
 describe Contact do
-  
+  describe '#initialize' do
+    it 'sets the attributes of the new contact' do
+      contact = Contact.new({
+        :first_name => 'John',
+        :last_name => 'Doe',
+        :job_title => 'Manager',
+        :company => 'ACME Inc',
+        :contact_type => 'business'
+      })
+      expect(contact.first_name()).to eq('John')
+      expect(contact.last_name()).to eq('Doe')
+      expect(contact.job_title()).to eq('Manager')
+      expect(contact.company()).to eq('ACME Inc')
+      expect(contact.contact_type()).to eq('business')
+    end
+  end
 end
