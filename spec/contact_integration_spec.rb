230b7e86fd8ccaa13938274e6443d45184f4ab5b
Roy Kim <rykim79@gmail.com>
2018-08-28 15:36:41 -0400

    add test coverage to integration

--- spec/contact_integration_spec.rb
+++ spec/contact_integration_spec.rb
@@ -24,4 +24,23 @@ describe 'the contact path', :type => :feature do
     expect(page).to have_css('li.contact', :text => 'John')
     expect(page).to have_css('li.contact', :text => 'Doe')
   end
+
+  it 'displays the contacts in the list of contacts that have been saved' do
+    fill_in :first_name, :with => 'John'
+    fill_in :last_name, :with => 'Doe'
+    fill_in :job_title, :with => 'Manager'
+    fill_in :company, :with => 'ACME Inc'
+    fill_in :contact_type, :with => 'Business'
+    click_button 'Add'
+    fill_in :first_name, :with => 'Mike'
+    fill_in :last_name, :with => 'Smith'
+    fill_in :job_title, :with => 'Manager'
+    fill_in :company, :with => 'ACME Inc'
+    fill_in :contact_type, :with => 'Business'
+    click_button 'Add'
+    expect(page).to have_css('li.contact', :text => 'John')
+    expect(page).to have_css('li.contact', :text => 'Doe')
+    expect(page).to have_css('li.contact', :text => 'Mike')
+    expect(page).to have_css('li.contact', :text => 'Smith')
+  end
 end
